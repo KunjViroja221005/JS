144b5ff0539d279a60f1223cb261e92224207356
KunjViroja221005 <kunjviroja221005@gmail.com>
2025-07-01 16:02:26 +0000

    04_1

---
+++ P01/04_Comparision.js
@@ -0,0 +1,16 @@
+// console.log("2" > 1);       //typescript doesnt allow diffrent datatypes to compare
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);      // equality check(==) and comparision (> ,<) works diffrently 
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// Avoid this type of comparisions
+
+// === : Strict Check (Checks the Data Type Also)
+
+console.log("2" == 2);
+console.log("2" === 2);
