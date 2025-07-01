fba5799c88cee5a005de83a1ce9c71dd47f88c17
KunjViroja221005 <kunjviroja221005@gmail.com>
2025-07-01 15:23:24 +0000

    02_Datatypes

---
+++ P01/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat whole code as newer version
+
+//alert("hello") can't run in vs code
+
+console.table([5-3,"kunj"])
+
+/*
+    number
+    big int
+    string => ""
+    boolean => true/false
+    null => standard value
+    undefined =>
+    symbol => unique
+    object
+*/
+console.log(typeof null);
+console.log(typeof undefined);
+
